a700dc8d577f83e5392339820abfbd9e85ea30f0
Luiz Tome <luizcarlostb@hotmail.com>
2024-06-26 13:40:34 -0300

    feat: atualizacao do formulário

--- src/components/form/ProjectFormLayout.tsx
+++ src/components/form/ProjectFormLayout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Typography } from "@mui/material";
+import { FormControl, InputLabel, MenuItem, Select, Typography } from "@mui/material";
 import TextField from "@mui/material/TextField";
 import { styled } from "styled-components";
 
@@ -10,7 +10,7 @@ export const Form = styled.form`
   justify-content: center;
   align-items: center;  
   background-color: #fbfbfb;
-  margin: 0 auto;
+  margin: 0 auto; 
 
   @media (max-width: 768px) {
     flex-direction: column;
@@ -63,4 +63,60 @@ export const CustomTextField = styled(TextField)`
 }
 `;
 
+export const CustomFormControl = styled(FormControl)`
+  margin: 0;
+  width: 810px;
+  color: #898989;  
+  border: 1.5px; 
+  border-radius: 8px;
+  border-color: #7C7C7C;  
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 24px;
+  letter-spacing: 0.15px;
+`;
+
+export const StyledInputLabel = styled(InputLabel)`
+  color: #898989;
+
+  &.Mui-focused {
+    color: #7C7C7C;
+  }
+`;
+
+export const StyledSelect = styled(Select)`
+  .MuiOutlinedInput-notchedOutline {
+    border-color: #7C7C7C;
+  }
+
+  &:hover .MuiOutlinedInput-notchedOutline {
+    border-color: #5C5C5C;
+  }
+
+  &.Mui-focused .MuiOutlinedInput-notchedOutline {
+    border-color: #5C5C5C;
+  }
+
+  .MuiSelect-icon {
+    color: #898989;
+  }
+`;
+
+export const StyledMenuItem = styled(MenuItem)`
+  background-color: #fbfbfb;
+
+&:hover {
+  background-color: #717171;
+}
+
+&.Mui-selected {
+  background-color: #717171;
+  color: #ffffff;
+}
+
+&.Mui-selected:hover {
+  background-color: #5C5C5C;
+}
+`;
 
