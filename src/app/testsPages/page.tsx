76cd2a0a50c2a02e7f79b8726ed5952906ecf1c2
Luiz Tome <luizcarlostb@hotmail.com>
2024-07-01 14:45:23 -0300

    feat: criar empty

--- src/app/testsPages/page.tsx
+++ src/app/testsPages/page.tsx
@@ -1,14 +1,18 @@
 "use client";
 
-import ProjectUploadFile from "@/components/upload/ProjectUpload";
+// import ProjectUploadFile from "@/components/upload/ProjectUpload";
 import { PageContainer } from "./layout";
-import ProjectLoadSuccess from "@/components/loading/ProjectLoadSuccess";
+// import ProjectLoadSuccess from "@/components/loading/ProjectLoadSuccess";
+import EmptyState from "@/components/empty/EmptyState";
+
+
 
 
 const TestsPage: React.FC = () => {
   return (
     <PageContainer>
-      <ProjectLoadSuccess />
+      {/* <ProjectLoadSuccess /> */}
+      <EmptyState />
       
     </PageContainer>
   );
