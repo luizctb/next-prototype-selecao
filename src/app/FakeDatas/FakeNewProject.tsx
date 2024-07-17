d3a39773c1381a1fac52a12098f09b9b6362290d
Luiz Tomé <luizcarlostb@hotmail.com>
2024-07-17 10:05:36 -0300

    Delete src directory

--- src/app/FakeDatas/FakeNewProject.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export const information02 = [
-   {
-      title: "Fazenda de Plantação de soja (Betim - MG)",
-      text: "Trabalho sendo realizado em fragmentos de restauração de fibra para retorno de onças pintadas para a região. Trabalho elaborado em parcerias ambientais.",
-      image: 'http://localhost:3000/images/fazenda01.png',      
-   }
-]
--- src/app/FakeDatas/FakeRegistration.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export const information = [
-   {
-      title: "Nenhum Projeto Cadastrado:",
-      text: "Cadastre algum projeto para continuar",
-      image: 'http://localhost:3000/images/oncaCard.png',      
-   }
-]
